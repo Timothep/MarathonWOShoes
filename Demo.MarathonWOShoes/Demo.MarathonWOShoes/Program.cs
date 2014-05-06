35d368c28ec551278ae81d0adef4ba9862ab11c1
Tim Bourguignon <timothepdotnet@gmail.com>
2014-05-06 21:00:05 +0200

    Initial Commit

---
+++ Demo.MarathonWOShoes/Demo.MarathonWOShoes/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RestSharp;
+using TinyIoC;
+
+namespace Demo.MarathonWOShoes
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			TestIoc();
+
+			////January 28th 2010 + 1 month = February 28th 2010
+			//Console.WriteLine(new DateTime(2010, 1, 28).AddMonths(1).ToShortDateString());
+
+			////January 29th 2010 + 1 month = February 28th 2010
+			//Console.WriteLine(new DateTime(2010, 1, 29).AddMonths(1).ToShortDateString());
+
+			////January 30th 2010 + 1 month = February 28th 2010
+			//Console.WriteLine(new DateTime(2010, 1, 30).AddMonths(1).ToShortDateString());
+
+			////February 28th 2010 - 1 month = January 28th 2010
+			//Console.WriteLine(new DateTime(2010, 2, 28).AddMonths(-1).ToShortDateString());
+
+			//Console.ReadLine();
+
+			///var client = new RestClient("http://example.com");
+
+		}
+
+		public DateTime AddTwoDays()
+		{
+			var now = System.DateTime.Now;
+			return now.AddDays(2);
+		}
+
+		public static void TestIoc()
+		{
+			var container = TinyIoCContainer.Current;
+			container.AutoRegister();
+			//var instance = container.Resolve<Ia>();
+
+			//container.Register<B>();
+			//container.Register<Ia>((c,p) => new A("something"));
+			var instance = container.Resolve<Ib>();
+		}
+	}
+
+
+	public interface Ia { }
+
+	public class A : Ia
+	{
+		private string a;
+		public A(string _a) { a = _a; }
+	}
+
+	public interface Ib {	}
+
+	public class B : Ib
+	{
+		private Ia a;
+		public B(Ia _a) { a = _a; }
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# MarathonWOShoes
+
+Presentation material for the [MarathonWOShoes](). This talk ...
+
+This material is/was used in various forms at different talks/conferences:
+
+* Karlsruhe Entwickler Tage, Karlsruhe, May 2014
+* [DWX](), Nürnberg, July 2014
+
+This talk was also written as an article (in German):
+
+* [KaffeeKlatsch](), "MarathonWOShoes", xxx 2014

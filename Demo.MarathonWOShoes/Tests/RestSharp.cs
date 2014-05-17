8583dcc483a60235366234583e9e6974082512ad
Tim Bourguignon <timothepdotnet@gmail.com>
2014-05-17 14:59:42 +0200

    Restsharp complete first path

--- Demo.MarathonWOShoes/Tests/RestSharp.cs
+++ Demo.MarathonWOShoes/Tests/RestSharp.cs
@@ -43,84 +43,119 @@ namespace Tests
 		[TestMethod]
 		public void TestRestSharp()
 		{
+			//Ex1
 			var client = new RestClient("url");
 			var request = new RestRequest("/", Method.GET);
 			var response = client.Execute(request);
 			Console.WriteLine(response.Content);
 
-			// Or async
-			//client.ExecuteAsync(request, response => {
-			//    Console.WriteLine(response.Content);
-			//});
+			//Ex2
+			client = new RestClient("http://example.com");
+			request = new RestRequest("resource/{id}", Method.POST);
+
+			//Ex3 Async
+			client.ExecuteAsync(request, resp => Console.WriteLine(resp.Content));
 		}
 
 		[TestMethod]
-		public void TestRestSharp2()
+		public void TestParameters()
 		{
-			//var client = new RestClient("http://example.com");
-			//var request = new RestRequest("resource/{id}", Method.POST);
+			var client = new RestClient("http://example.com");
+			var request = new RestRequest("resource/{id}", Method.POST);
 
-			//// --------------------
+			// adds to POST or URL querystring based on Method
+			request.AddParameter("name", "value");
 
-			//// adds to POST or URL querystring based on Method
-			//request.AddParameter("name", "value"); 
+			// replaces matching token in request.Resource
+			request.AddUrlSegment("id", "123");
 
-			//// replaces matching token in request.Resource
-			//request.AddUrlSegment("id", "123"); 
+			// add parameters for all properties on an object
+			request.AddObject(new MyInt{myInt = 42});
 
-			//// add parameters for all properties on an object
-			//request.AddObject(object);
+			// add files to upload (works with compatible verbs)
+			// may throw a FileNotFoundException
+			request.AddFile("name", "path"); 
+		}
 
-			//// or just whitelisted properties
-			//request.AddObject(object, "PersonId", "Name", ...);
+		internal class MyInt
+		{
+			public int myInt { get; set; }
+		}
 
-			//// add files to upload (works with compatible verbs)
-			//request.AddFile("path");
+		[TestMethod]
+		public void TestHeaders()
+		{
+			var client = new RestClient("url");
+			var request = new RestRequest("/", Method.GET);
 
-			//// --------------------
+			// easily add HTTP Headers
+			request.AddHeader("header", "value");
 
-			//// easily add HTTP Headers
-			//request.AddHeader("header", "value");
+		}
 
-			//// --------------------
+		[TestMethod]
+		public void TestExecution()
+		{
+			var client = new RestClient("url");
+			var request = new RestRequest("/", Method.GET);
 
-			//// execute the request
-			//IRestResponse response = client.Execute(request);
-			//var content = response.Content; // raw content as string
+			// execute the request
+			IRestResponse response = client.Execute(request);
+			var content = response.Content; // raw content as string
 
-			//// easy async support
-			//client.ExecuteAsync(request, response => {
-			//		Console.WriteLine(response.Content);
-			//});
+			// easy async support
+			client.ExecuteAsync(request, resp => Console.WriteLine(resp.Content));
 
+		}
 
+		[TestMethod]
+		public void TestSerialize()
+		{
+			var client = new RestClient("url");
+			var request = new RestRequest("/", Method.GET);
 
-			//// --------------------
+			// return content type is sniffed but can be explicitly set via RestClient.AddHandler();
+			IRestResponse<Person> response = client.Execute<Person>(request);
+			var name = response.Data.Name;
+		}
 
-			//// return content type is sniffed but can be explicitly set via RestClient.AddHandler();
-			//IRestResponse<Person> response = client.Execute<Person>(request);
-			//var name = response.Data.Name; 
+		[TestMethod]
+		public void TestDeserialize()
+		{
+			var client = new RestClient("url");
+			var request = new RestRequest("/", Method.GET);
 
-			//// async with deserialization
-			//var asyncHandle = client.ExecuteAsync<Person>(request, response => {
-			//		Console.WriteLine(response.Data.Name);
-			//});
+			// async with deserialization
+			var asyncHandle = client.ExecuteAsync<Person>(request, resp => Console.WriteLine(resp.Data.Name));
 
-			//asyncHandle.Abort();
+			asyncHandle.Abort();
+		}
 
-			//// --------------------
+		internal class Person
+		{
+			public bool Name { get; set; }
+		}
 
-			//client.DownloadData(request).SaveAs(path);
+		[TestMethod]
+		public void TestDownload()
+		{
+			var client = new RestClient("url");
+			var request = new RestRequest("/", Method.GET);
 
-			//string tempFile = Path.GetTempFileName();
-			//using (var writer = File.OpenWrite(tempFile))
-			//{
-			//		var client = new RestClient(baseUrl);
-			//		var request = new RestRequest("Assets/LargeFile.7z");
-			//		request.ResponseWriter = (responseStream) => responseStream.CopyTo(writer);
-			//		var response = client.DownloadData(request);
-			//}
+			client.DownloadData(request).SaveAs("path");
+		}
 
+		[TestMethod]
+		public void TestStreaming()
+		{
+			string tempFile = Path.GetTempFileName();
+			using (var writer = File.OpenWrite(tempFile))
+			{
+				var client = new RestClient("baseUrl");
+				var request = new RestRequest("Assets/LargeFile.7z");
+				request.ResponseWriter = (responseStream) => responseStream.CopyTo(writer);
+				var response = client.DownloadData(request);
+			}
 		}
 	}
 }
